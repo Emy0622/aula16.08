31473b2422c91fd3a33ba90edd3d4e62d20f90e4
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2023-08-16 12:27:23 -0300

    meio

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const botaoSomar = document.getElementById('somar')
 const botaoIdentificar = document.getElementById('identificar')
+const botaoCalcularM = document.getElementById('calcular')
+
 
 botaoSomar.addEventListener('click', somar)
 
@@ -15,13 +17,17 @@ function somar(){
 
     resultado.textContent = total
 }
+
+
+botaoIdentificar.addEventListener('click', identificar)
+
 function identificar() {
 const numero = Number (document.getElementById('numero-item2').value)
 const resultado = document.getElementById('resultado-item2')
 let msg
 if (numero > 0) {
     msg = 'positivo'
-}else if(numero > 0){
+}else if(numero < 0){
     msg = 'negativo'
 }else{
     msg = 'zero'
@@ -30,4 +36,19 @@ if (numero > 0) {
 resultado.textContent = msg
 
 }
-botaoIdentificar.addEventListener('click', identificar)
+
+botaoCalcularM.addEventListener('click', calcular)
+function calcular() {
+    const bi1 = parseInt(document.getElementById('n1').value)
+    const bi2 = parseInt(document.getElementById('n2').value)
+    const bi3 = parseInt(document.getElementById('n3').value)
+    const bi4 = parseInt(document.getElementById('n4').value)
+
+    const resultado = document.getElementById('resultadoM')
+
+    const total = numero1 + numero2
+
+    resultado.textContent = total
+
+}
+
